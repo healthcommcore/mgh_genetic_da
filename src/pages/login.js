75f3617056254970fec05fc3e836e55f2575c5fd
David Rothfarb <david@dr809.dfci.harvard.edu>
2020-03-05 12:41:30 -0500

    Installed redux-form, created FieldInput component to be wrapped by redux-form components, replaced login fields with new redux-form fields

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,6 +2,8 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
+import FieldInput from "../components/field-input";
+import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
 
 const mapDispatchToProps = (dispatch) => {
@@ -14,7 +16,7 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-const Login = ({ data, setUser }) => {
+let Login = ({ data, setUser }) => {
   const cancers = data.allTaxonomyTermCancerType.edges;
   const sites = data.allTaxonomyTermSites.edges;
   return (
@@ -22,35 +24,39 @@ const Login = ({ data, setUser }) => {
       <Form className="login" onSubmit={ setUser }>
         <Form.Group controlId="patientID">
           <Form.Label>Enter patient ID</Form.Label>
-          <Form.Control size="lg" type="text" />
+          <Field name="userid" component={ FieldInput } />
+          {/*<Form.Control size="lg" type="text" />*/}
+
         </Form.Group>
         <Form.Group controlId="cancerType">
           <Form.Label>Select cancer type</Form.Label>
-          <Form.Control size="lg" as="select">
+          <Field name="cancer_type" size="lg" as="select" component={ FieldInput }>
             <option>--Select cancer--</option>
             { cancers.map( (cancer, i) => {
               return (
                 <option key={ i }>{ cancer.node.name }</option>
               )
             })}
-          </Form.Control>
+          </Field>
+         </Form.Group>
          <Form.Group controlId="sites">
           <Form.Label>Select site</Form.Label>
-          <Form.Control size="lg" as="select">
+          <Field name="site" size="lg" as="select" component={ FieldInput }>
             <option>--Select site--</option>
             { sites.map( (site, i) => {
               return (
                 <option key={ i }>{ site.node.name }</option>
               )
             })}
-          </Form.Control>
+          </Field>
         </Form.Group>
-       </Form.Group>
        <Button variant="primary" size="lg" type="submit">Submit</Button>
       </Form>
     </Container>
   );
 }
+
+Login = reduxForm({ form: "login" })(Login);
 
 export const query = graphql`
   query {
--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -4,8 +4,11 @@ import { createStore } from "redux";
 import user from "./src/reducers/user";
 
 const store = createStore(
+  user
+  /*
   user,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  */
 );
 
 export default ({ element }) => {
