6fab694a522d106d866fbc69d5da520c1895bf9d
Dave Rothfarb <dave.rothfarb.webdev@gmail.com>
2020-05-28 17:05:36 -0400

    Page title design

--- src/components/page-title.js
+++ src/components/page-title.js
@@ -4,9 +4,11 @@ import Container from "react-bootstrap/Container";
 const PageTitle = ({ children }) => {
   return (
     <div className="title-border">
-      <Container>
-        <h1>{ children }</h1>
-      </Container>
+      <div className="title-bkgrd">
+        <Container>
+          <h1>{ children }</h1>
+        </Container>
+      </div>
     </div>
   );
 }
