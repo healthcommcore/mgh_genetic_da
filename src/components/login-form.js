419e07b5157897d11a8d6a4959ab104ac4c27dae
Dave Rothfarb <dave.rothfarb.webdev@gmail.com>
2020-06-10 14:40:08 -0400

    Login form cleanup

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import ContentContainer from "../components/content-container";
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
 import FieldInput from "./field-input";
@@ -11,49 +12,53 @@ let LoginForm = ({ cancers, sites, handleSubmit }) => {
   const [match, confirmMatch] = useState(false);
   const [show, showAlert] = useState(false);
   return (
-    <Container>
-      <Form className="login" onSubmit={ handleSubmit }>
-        <Form.Group controlId="patientID">
-          <Form.Label>Enter patient ID</Form.Label>
-          <Field name="userid" component={ FieldInput } onChange={ (e) => updateId(e.target.value) }/>
-        </Form.Group>
-        <Alert variant={ match ? "success" : "danger" } show={ show }>
-          The two patient ids { !match && "do not" } match!
-        </Alert>
-        <Form.Group controlId="patientIDConfirm">
-          <Form.Label>Re-enter patient ID</Form.Label>
-          <Form.Control 
-            name="useridconfirm" 
-            type="text"
-            component={ FieldInput } 
-            onChange={ (e) => confirmMatch(e.target.value === userid) }
-            onFocus={ () => showAlert(true) }
-          />
-        </Form.Group>
-        <Form.Group controlId="cancerType">
-          <Form.Label>Select cancer type</Form.Label>
-          <Field name="cancerType" size="lg" as="select" component={ FieldInput }>
-            <option>--Select cancer--</option>
-            { cancers.map( (cancer, i) => {
-              return (
-                <option key={ i }>{ cancer.node.name }</option>
-              )
-            })}
-          </Field>
-         </Form.Group>
-         <Form.Group controlId="sites">
-          <Form.Label>Select site</Form.Label>
-          <Field name="site" size="lg" as="select" component={ FieldInput }>
-            <option>--Select site--</option>
-            { sites.map( (site, i) => {
-              return (
-                <option key={ i }>{ site.node.name }</option>
-              )
-            })}
-          </Field>
-        </Form.Group>
-       <Button variant="primary" size="lg" type="submit">Submit</Button>
-      </Form>
+    <Container bsPrefix="container mt-5">
+      <ContentContainer>
+        <Form className="login" onSubmit={ handleSubmit }>
+          <Form.Group controlId="patientID">
+            <Form.Label>Enter patient ID</Form.Label>
+            <Field name="userid" component={ FieldInput } onChange={ (e) => updateId(e.target.value) }/>
+          </Form.Group>
+          <Alert variant={ match ? "success" : "danger" } show={ show }>
+            The two patient ids { !match && "do not" } match!
+          </Alert>
+          <Form.Group controlId="patientIDConfirm">
+            <Form.Label>Re-enter patient ID</Form.Label>
+            <Form.Control 
+              name="useridconfirm" 
+              type="text"
+              component={ FieldInput } 
+              onChange={ (e) => confirmMatch(e.target.value === userid) }
+              onFocus={ () => showAlert(true) }
+            />
+          </Form.Group>
+          <Form.Group controlId="cancerType">
+            <Form.Label>Select cancer type</Form.Label>
+            <Field name="cancerType" size="lg" as="select" component={ FieldInput }>
+              <option>--Select cancer--</option>
+              { cancers.map( (cancer, i) => {
+                return (
+                  <option key={ i }>{ cancer.node.name }</option>
+                )
+              })}
+            </Field>
+           </Form.Group>
+           <Form.Group controlId="sites">
+            <Form.Label>Select site</Form.Label>
+            <Field name="site" size="lg" as="select" component={ FieldInput }>
+              <option>--Select site--</option>
+              { sites.map( (site, i) => {
+                return (
+                  <option key={ i }>{ site.node.name }</option>
+                )
+              })}
+            </Field>
+          </Form.Group>
+          <div className="text-center">
+           <Button variant="da rounded-pill mt-3" size="lg" type="submit">Submit</Button>
+          </div>
+        </Form>
+      </ContentContainer>
     </Container>
   );
 }
