ad8dc4e5789f331097bec540fa96e09f3728f3d7
Dave Rothfarb <dave.rothfarb.webdev@gmail.com>
2020-07-16 17:10:01 -0400

    In the middle of adding a confirm modal to the email submitter component

--- src/components/email-submitter.js
+++ src/components/email-submitter.js
@@ -1,64 +1,95 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import Form from "react-bootstrap/Form"
 import Col from "react-bootstrap/Col"
 import Button from "react-bootstrap/Button"
+import Modal from "react-bootstrap/Modal"
 import PropTypes from "prop-types";
 
-const sendEmail = (data, email = false, notes = false) => {
-  const payload = {
-    email,
-    userBasic: {
-      id: data.userid,
-      cancerType: data.cancerType,
-      site: data.site
-    },
-    userTest : {
-      decision: data.test.doYouWantGeneticTest,
-      test: data.test.testTypes || "no test selected",
-      nextSteps: data.test.notReadyToDecide && data.test.notReadyToDecide.length > 0 && data.test.notReadyToDecide.join(", ")
-    },
-    userValues: data.values,
-    notes
-  };
-  const json = JSON.stringify(payload);
-  fetch("http://api.geneticda.hccstaging.com/sendmail.php",
-    {
-      method: "post",
-      headers: { 
-        "Content-Type" : "application/json",
-        "Accept" : "application/json" 
+
+
+class EmailSubmitter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: false,
+      confirm: false
+    };
+    this.sendEmail = this.sendEmail.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  sendEmail = (data, email = false, notes = false) => {
+    const payload = {
+      email,
+      userBasic: {
+        id: data.userid,
+        cancerType: data.cancerType,
+        site: data.site
       },
-      body: json
-    })
-    .then( (response) => {
-      console.log(response);
-    })
-    .catch( (err) => {
-      console.error("Error:", err);
-    });
+      userTest : {
+        decision: data.test.doYouWantGeneticTest,
+        test: data.test.testTypes || "no test selected",
+        nextSteps: data.test.notReadyToDecide && data.test.notReadyToDecide.length > 0 && data.test.notReadyToDecide.join(", ")
+      },
+      userValues: data.values,
+      notes
+    };
+    const json = JSON.stringify(payload);
+    fetch("http://api.geneticda.hccstaging.com/sendmail.php",
+      {
+        method: "post",
+        headers: { 
+          "Content-Type" : "application/json",
+          "Accept" : "application/json" 
+        },
+        body: json
+      })
+      .then( (response) => {
+        console.log(response);
+        this.setState({ confirm: true });
+      })
+      .catch( (err) => {
+        console.error("Error:", err);
+      });
+  }
+
+  setEmail = (value) => {
+    this.setState({ email: value });
+  }
+
+  handleClose = () => {
+    this.setState({ confirm: false });
+  }
+
+  render() {
+    return (
+      <>
+        <Modal show={ this.confirm } onHide={ this.handleClose }>
+          <Modal.Body>Your email has been successfully sent</Modal.Body>
+          <Modal.Footer>
+           <Button variant="da rounded-pill" onClick={ this.handleClose }>Ok</Button> 
+          </Modal.Footer>
+        </Modal>
+        <Form>
+          <div className="d-flex justify-content-around">      
+              { this.props.type === "user" ? 
+                <Form.Control 
+                  placeholder="Enter email address"
+                  type="email"
+                  onChange={ (e) => this.setEmail(e.target.value) }  
+                /> : "" 
+              } 
+            <div>
+              <Button variant="da rounded-pill" onClick={ () => this.sendEmail(this.props.data, this.props.email, this.props.notes) }>{ this.props.children }</Button>
+            </div>
+          </div>      
+        </Form>
+      </>
+    )
+  }
 }
 
-const EmailSubmitter = ({ type, data, children }) => {
-  const [email, setEmail] = useState("");
-  // Only pass the user notes if the email is going to the user, not to admin
-  const notes = (type === "user" ? data.notes : false);
-  return (
-    <Form>
-      <div className="d-flex justify-content-around">      
-          { type === "user" ? 
-            <Form.Control 
-              placeholder="Enter email address"
-              type="email"
-              onChange={ (e) => setEmail(e.target.value) }  
-            /> : "" 
-          } 
-        <div>
-          <Button variant="da rounded-pill" onClick={ () => sendEmail(data, email, notes) }>{ children }</Button>
-        </div>
-      </div>      
-    </Form>
-  )
-}
 
 EmailSubmitter.propTypes = {
   type: PropTypes.string,
